b2403598f3a24e8370c649099b620a9ad21c9384
salarraq1995-cpu <salarraq1995@gmail.com>
2026-07-23 20:30:21 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,39 +1,51 @@
 const matchesDiv = document.getElementById("matches");
 
 async function loadMatches() {
-    try {
-        const response = await fetch("https://api.openligadb.de/getmatchdata/bl1");
-        const data = await response.json();
 
-        matchesDiv.innerHTML = "";
+    const response = await fetch(
+        "https://goaal-live-api.joud07511.workers.dev"
+    );
 
-        data.forEach(match => {
-            matchesDiv.innerHTML += `
-                <div class="card">
-                    <div class="league">🇩🇪 الدوري الألماني</div>
+    const data = await response.json();
 
-                    <div class="match">
-                        <div class="team">${match.Team1.TeamName}</div>
+    matchesDiv.innerHTML = "";
 
-                        <div class="score">
-                            ${match.MatchResults?.length
-                                ? `${match.MatchResults[0].PointsTeam1} - ${match.MatchResults[0].PointsTeam2}`
-                                : "vs"}
-                        </div>
+    data.forEach(match => {
 
-                        <div class="team">${match.Team2.TeamName}</div>
-                    </div>
+        matchesDiv.innerHTML += `
 
-                    <div class="time">
-                        ${new Date(match.MatchDateTime).toLocaleString("ar-IQ")}
-                    </div>
+        <div class="card">
+
+            <div class="league">
+            ⚽ الدوري الألماني
+            </div>
+
+            <div class="match">
+
+                <div class="team">
+                ${match.Team1.TeamName}
                 </div>
-            `;
-        });
 
-    } catch (e) {
-        matchesDiv.innerHTML = "<p>❌ تعذر تحميل المباريات</p>";
-    }
+                <div class="score">
+                ${match.MatchResults?.[0]
+                ? match.MatchResults[0].PointsTeam1 + " - " + match.MatchResults[0].PointsTeam2
+                : "vs"}
+                </div>
+
+                <div class="team">
+                ${match.Team2.TeamName}
+                </div>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
 }
+
+loadMatches();
 
 loadMatches();
