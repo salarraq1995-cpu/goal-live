1be91c1c2765ed855f3b37f859b81b1e2a769925
salarraq1995-cpu <salarraq1995@gmail.com>
2026-07-23 18:40:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const tabs = document.querySelectorAll(".tab");
 
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(t => {
+            t.classList.remove("active");
+        });
+
+        tab.classList.add("active");
+
+    });
+
+});
+
+
+
+const search = document.querySelector(".search");
+const cards = document.querySelectorAll(".card");
+
+
+search.addEventListener("input", () => {
+
+    let value = search.value.toLowerCase();
+
+
+    cards.forEach(card => {
+
+        let text = card.innerText.toLowerCase();
+
+
+        if(text.includes(value)){
+            card.style.display="block";
+        }
+        else{
+            card.style.display="none";
+        }
+
+    });
+
+
+});
