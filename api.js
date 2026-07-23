acb2ff08efee89219f123719182a796c2ebcf19a
salarraq1995-cpu <salarraq1995@gmail.com>
2026-07-23 19:02:21 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,13 @@
-const API_KEY = "1";
+async function getMatches(){
+
+    const url = "https://api.openligadb.de/getmatchdata/bl1/2026";
+
+    const response = await fetch(url);
+
+    const matches = await response.json();
+
+    console.log(matches);
+
+}
+
+getMatches();
