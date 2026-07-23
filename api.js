aa17a922cf63281b63774e62fdac316f769b395a
salarraq1995-cpu <salarraq1995@gmail.com>
2026-07-23 20:02:31 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,39 @@
-alert("api.js شغال");
+const matchesDiv = document.getElementById("matches");
+
+async function loadMatches() {
+    try {
+        const response = await fetch("https://api.openligadb.de/getmatchdata/bl1");
+        const data = await response.json();
+
+        matchesDiv.innerHTML = "";
+
+        data.forEach(match => {
+            matchesDiv.innerHTML += `
+                <div class="card">
+                    <div class="league">🇩🇪 الدوري الألماني</div>
+
+                    <div class="match">
+                        <div class="team">${match.Team1.TeamName}</div>
+
+                        <div class="score">
+                            ${match.MatchResults?.length
+                                ? `${match.MatchResults[0].PointsTeam1} - ${match.MatchResults[0].PointsTeam2}`
+                                : "vs"}
+                        </div>
+
+                        <div class="team">${match.Team2.TeamName}</div>
+                    </div>
+
+                    <div class="time">
+                        ${new Date(match.MatchDateTime).toLocaleString("ar-IQ")}
+                    </div>
+                </div>
+            `;
+        });
+
+    } catch (e) {
+        matchesDiv.innerHTML = "<p>❌ تعذر تحميل المباريات</p>";
+    }
+}
+
+loadMatches();
