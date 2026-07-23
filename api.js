d0cac3ef398329cf60fedd5401eedf9634197bc7
salarraq1995-cpu <salarraq1995@gmail.com>
2026-07-23 19:12:27 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,57 @@
 async function getMatches(){
 
-    const url = "https://api.openligadb.de/getmatchdata/bl1/2026";
+const url = "https://api.openligadb.de/getmatchdata/bl1/2026";
 
-    const response = await fetch(url);
+const response = await fetch(url);
 
-    const matches = await response.json();
+const matches = await response.json();
 
-    console.log(matches);
+
+const box = document.getElementById("matches");
+
+
+matches.forEach(match => {
+
+box.innerHTML += `
+
+<div class="card">
+
+<div class="league">
+🏆 الدوري الألماني
+</div>
+
+<div class="match">
+
+<div class="team">
+${match.team1.teamName}
+</div>
+
+<div>
+
+<div class="score">
+${match.matchResults?.[0]?.pointsTeam1 || 0} - 
+${match.matchResults?.[0]?.pointsTeam2 || 0}
+</div>
+
+<div class="time">
+${match.matchDateTime}
+</div>
+
+</div>
+
+<div class="team">
+${match.team2.teamName}
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
 
 }
+
 
 getMatches();
